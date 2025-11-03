2002e7b38fcab0ed374f42e12a6c1890c40ea248
mohamadrayenjbili <mohamadrayen.jbili@esprit.tn>
2025-11-03 09:35:01 +0100

    update du projet

--- src/main/java/tn/esprit/chambre/repository/UniversiteRepository.java
+++ src/main/java/tn/esprit/chambre/repository/UniversiteRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.chambre.entities.Universite;
 
+import java.util.List;
+
 @Repository
 public interface UniversiteRepository extends JpaRepository<Universite, Long> {
+    List<Universite> findByNomContaining(String nom);
+    List<Universite> findByAdresseContaining(String adresse);
 }
